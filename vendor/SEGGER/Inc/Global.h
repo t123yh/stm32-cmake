1b571852ed0338e2ddee3ea6d9473beba8978433
Yunhao Tian <t123yh@outlook.com>
2020-06-15 09:25:39 +0800

    Add patched FreeRTOS for SystemView support.

---
+++ vendor/SEGGER/Inc/Global.h
@@ -0,0 +1,14 @@
+#ifndef __SEGGER_GLOBAL_H__ 
+#define __SEGGER_GLOBAL_H__
+
+#include <stdint.h>
+typedef int8_t I8;
+typedef int16_t I16;
+typedef int32_t I32;
+typedef int16_t I64;
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint16_t U64;
+
+#endif
